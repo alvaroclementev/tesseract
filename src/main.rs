d10398baf83bb39bd741a3326532cb227864372c
Alvaro Clemente <clemente.varo@gmail.com>
2022-06-03 20:04:11 +0200

    feat: add and test basic dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+extern crate dotenv;
+use dotenv::dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let twitter_api_key =
+        std::env::var("TWITTER_API_KEY").expect("TWITTER_API_KEY env var should be set");
+    let twitter_api_key_secret =
+        std::env::var("TWITTER_API_KEY_SECRET").expect("TWITTER_API_KEY_SECRET env var should be set");
+    println!("{} ({})", &twitter_api_key, &twitter_api_key_secret);
 }
